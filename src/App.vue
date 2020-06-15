2f931498d6025e76ff7a9b45cf0538df6bed26a3
James Stevenson <info@presencewebdesign.co.uk>
2020-06-15 11:19:03 +0100

    make change

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="container">
     <SearchBar @termChange="onTermChange"></SearchBar>
-
     <div class="row">
       <VideoDetail :video="selectedVideo" />
       <VideoList :videos="videos" @videoSelect="onVideoSelect"></VideoList>
